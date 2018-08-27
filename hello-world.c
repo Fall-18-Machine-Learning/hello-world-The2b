e099ef801760eb30e1e5cf6bc309bcdab350273b
Thomas Lenz <thomas.lenz96@gmail.com>
2018-08-27 15:05:36 -0500

    First assignment complete

---
+++ hello-world.c
@@ -0,0 +1,15 @@
+/**
+ * @author Thomas Lenz <thomas.lenz96@gmail.com> AS The2b
+ * @date 2018-08-27
+ * @project Hello World
+ * @file hello-world.c
+ *
+ * The first assignment for Machine Learning, CSPS 47100
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!\n\0");
+	return 0;
+}
